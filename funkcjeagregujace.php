9c0c7f10930a028aa69831a4c52d0e11eedaadff
Matuszek Tomasz <73532403+matuchpv7@users.noreply.github.com>
2020-11-09 16:00:00 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -1,4 +1,22 @@
-<?php
-echo("tu jestem: Funkcje Agregujące");
-
-?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Matuszek</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h3><a href="https://github.com/SK-2019/PHP-SQL-Wprowadzenie-Matuszek-Tomasz">GITHUB</h3>
+        <header>
+          <div class="tomek">
+           <div class="navigator">
+                 <nav>
+            <a class="nav1" href="index.php">Strona główna</a>
+            <a class="nav1" href="pracownicy.php">Pracownicy</a>
+            <a class="nav1" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
+            <a class="nav1 active" href="funkcjeagregujace.php">Funkcje Agregujące</a>
+                </nav>
+           </div>
+    </body>
+</html>
