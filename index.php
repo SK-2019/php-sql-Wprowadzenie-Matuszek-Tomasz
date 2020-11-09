9cecf2d0dcbeb243bee4d163b8814e0b8a9374a2
Matuszek Tomasz <73532403+matuchpv7@users.noreply.github.com>
2020-11-09 13:49:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="pl">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>PHP - Matuszek</title>
-            <link rel="stylesheet" href="style.css">
-        </head>
-        <h3><a href="https://github.com/SK-2019/PHP-SQL-Wprowadzenie-Matuszek-Tomasz">GITHUB</h3>
-     <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Matuszek</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h3><a href="https://github.com/SK-2019/PHP-SQL-Wprowadzenie-Matuszek-Tomasz">GITHUB</h3>
         <header>
-         <div class="nav">
-            <a class="nav1 active" href="index.php">Strona główna</a>
-            <a class="nav1" href="pracownicy.php">Pracownicy</a>
-            <a class="nav1" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
-            <a class="nav1" href="funkcjeagregujace.php">Funkcje Agregujące</a>
-         </div>
+           <ul class="nav>
+            <li><a class="nav1 active" href="index.php">Strona główna</a></li>
+            <li><a class="nav1" href="pracownicy.php">Pracownicy</a></li>
+            <li><a class="nav1" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a></li>
+            <li><a class="nav1" href="funkcjeagregujace.php">Funkcje Agregujące</a></li>
+           </ul>
        </header>
     </body>
 </html>
