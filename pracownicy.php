4d10a44e367f10bebb0ee6198f7c01e03cc82ae5
Matuszek Tomasz <73532403+matuchpv7@users.noreply.github.com>
2020-11-09 18:48:09 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -19,6 +19,24 @@
             <a class="nav1" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
             <a class="nav1" href="funkcjeagregujace.php">Funkcje Agregujące</a>
                 </nav>
+           <h2 class="h2zb">Zad.1 - Select * FROM pracownicy WHERE dzial=2</h2>
+        <?php
+                require_once("connect.php");
+                $result=$conn->query("Select * From pracownicy where dzial=2");
+                echo("<table border=1>");
+                    echo("<th>ID</th>");
+                    echo("<th>Imie</th>");
+                    echo("<th>Dzial</th>");
+                    echo("<th>Nazwa_dzial</th>");
+                    echo("<th>Zarobki</th>");
+                    echo("<th>Data_Urodzenia</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+            ?>
+               
            </div>
       </body>
 </html>
