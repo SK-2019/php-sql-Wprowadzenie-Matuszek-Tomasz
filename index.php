33552f4a211b13ebfb1dbf8c2f99c966531e4369
Matuszek Tomasz <73532403+matuchpv7@users.noreply.github.com>
2020-11-09 12:11:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Matuszek</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>PHP - Matuszek</title>
+            <link rel="stylesheet" href="style.css">
+        </head>
+    <body>
     <div id="gg">
         <nav>
             <a class="nav1 active" href="index.php">Strona główna</a>
