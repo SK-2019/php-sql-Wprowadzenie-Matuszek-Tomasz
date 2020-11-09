09d897b67df8ee5d79a9b70aa4923629f993dc6e
Matuszek Tomasz <73532403+matuchpv7@users.noreply.github.com>
2020-11-09 18:38:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -101,8 +101,8 @@
                             echo("</tr>");}
                 echo("</table>");
           ?>
-                  <footer>PHP-SQL-Wprowadzenie-Matuszek Tomasz</footer>
               </div>
+              <footer>PHP-SQL-Wprowadzenie-Matuszek Tomasz</footer>
            </div>
        </header>
     </body>
