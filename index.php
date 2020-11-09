4b25eb85df0776f842901662ae9c162d30595669
Matuszek Tomasz <73532403+matuchpv7@users.noreply.github.com>
2020-11-09 12:25:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,11 @@
             <link rel="stylesheet" href="style.css">
 </head>
     <body>
-    <div id="gg">
-        <nav>
+         <div id="nav">
             <a class="nav1 active" href="index.php">Strona główna</a>
-            <a class="nav1" href="https://github.com/SK-2019/php-sql-Wprowadzenie-Matuszek-Tomasz">GITHUB</a>
             <a class="nav1" href="pracownicy.php">Pracownicy</a>
             <a class="nav1" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
             <a class="nav1" href="funkcjeagregujace.php">Funkcje Agregujące</a>
-        </nav>
-    </div>
-</body>
+         </div>
+    </body>
 </html>
