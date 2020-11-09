ae9932842c6f457f08b94d901527942121b3c5bf
Matuszek Tomasz <73532403+matuchpv7@users.noreply.github.com>
2020-11-09 12:29:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="pl">
-<head>
+        <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>PHP - Matuszek</title>
             <link rel="stylesheet" href="style.css">
-</head>
-    <body>
+        </head>
          <div id="nav">
             <a class="nav1 active" href="index.php">Strona główna</a>
             <a class="nav1" href="pracownicy.php">Pracownicy</a>
             <a class="nav1" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
             <a class="nav1" href="funkcjeagregujace.php">Funkcje Agregujące</a>
          </div>
+       <body>
     </body>
 </html>
