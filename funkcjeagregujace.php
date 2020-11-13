734128bc4dbba253d8631447357014d11d717943
a <73532403+matuchpv7@users.noreply.github.com>
2020-11-13 11:39:08 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -137,7 +137,107 @@
                             echo("</tr>");
                         }
                 echo("</table>");
-        ?>                    
+        ?>
+                <h2 class="h2za">Select sum(zarobki) as sz from pracownicy group by dzial</h2>
+        <?php
+                require_once("connect.php");
+                $result=$conn->query("Select sum(zarobki) as sz from pracownicy group by dzial");
+                echo("<table border=1>");
+                    echo("<th>sum(zarobki)</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["sz"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+            ?>
+            <h2 class="h2za">Select count(imie) as ci from pracownicy group by dzial</h2>
+        <?php
+                require_once("connect.php");
+                $result=$conn->query("Select count(imie) as ci from pracownicy group by dzial");
+                echo("<table border=1>");
+                    echo("<th>count(imie)</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["ci"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+            ?>
+            <h2 class="h2za">Select avg(zarobki) as az from pracownicy group by dzial</h2>
+        <?php
+                require_once("connect.php");
+                $result=$conn->query("Select avg(zarobki) as az from pracownicy group by dzial");
+                echo("<table border=1>");
+                    echo("<th>avg(zarobki)</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["az"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+            ?>
+            <h2 class="h2za">Select sum(zarobki) as sz from pracownicy group by imie not like '%a'</h2>
+        <?php
+                require_once("connect.php");
+                $result=$conn->query("Select sum(zarobki) as sz from pracownicy group by imie not like '%a'");
+                echo("<table border=1>");
+                    echo("<th>sum(zarobki)</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["sz"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+            ?>
+            <h2 class="h2za">Select avg(zarobki) as az from pracownicy group by imie not like '%a'</h2>
+        <?php
+                require_once("connect.php");
+                $result=$conn->query("Select avg(zarobki) as az from pracownicy group by imie not like '%a'");
+                echo("<table border=1>");
+                    echo("<th>avg(zarobki)</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["az"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+            ?>
+           <h2 class="h2gl">HAVING BY</h2>
+            <h2 class="h2za">Select sum(zarobki) as sz, nazwa_dzial from pracownicy, organizacja where dzial=id_org group by nazwa_dzial having sum(zarobki)<28</h2>
+        <?php
+                require_once("connect.php");
+                $result=$conn->query("Select sum(zarobki) as sz, nazwa_dzial from pracownicy, organizacja where dzial=id_org group by nazwa_dzial having sum(zarobki)<28");
+                echo("<table border=1>");
+                    echo("<th>sum(zarobki)</th>");
+                    echo("<th>nazwa_dzial</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["sz"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+            ?>
+            <h2 class="h2za">Select avg(zarobki) as az, nazwa_dzial from pracownicy, organizacja where dzial=id_org group by nazwa_dzial having avg(zarobki)>30</h2>
+        <?php
+                require_once("connect.php");
+                $result=$conn->query("Select avg(zarobki) as az, nazwa_dzial from pracownicy, organizacja where dzial=id_org group by nazwa_dzial having avg(zarobki)>30");
+                echo("<table border=1>");
+                    echo("<th>avg(zarobki)</th>");
+                    echo("<th>nazwa_dzial</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["az"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+            ?>
+            <h2 class="h2za">Select count(imie) as ci, nazwa_dzial from pracownicy, organizacja where dzial=id_org group by nazwa_dzial having avg(zarobki)>2</h2>
+        <?php
+                require_once("connect.php");
+                $result=$conn->query("Select count(imie) as ci, nazwa_dzial from pracownicy, organizacja where dzial=id_org group by nazwa_dzial having avg(zarobki)>2");
+                echo("<table border=1>");
+                    echo("<th>count(imie)</th>");
+                    echo("<th>nazwa_dzial</th>");
+                        while($row=$result->fetch_assoc()){
+                            echo("<tr>");
+                            echo("<td>".$row["ci"]."</td><td>".$row["nazwa_dzial"]."</td>");
+                            echo("</tr>");}
+                echo("</table>");
+            ?>
                </div>
              <footer>PHP-SQL-Wprowadzenie-Matuszek Tomasz</footer>
            </div>
