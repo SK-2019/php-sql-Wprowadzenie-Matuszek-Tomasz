3c9c130a5f9a7e36f6c3868faa8969dfb4091864
Bucky Roberts <matuszek.tomasz1@gmail.com>
2020-11-13 13:33:02 +0100

    gfd

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Matuszek</title>
+    <title>PHP - hehehehhehehe</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
