7194c2cf2afa6950cbae7e857e5dc5130ff94912
Bucky Roberts <matuszek.tomasz1@gmail.com>
2021-02-04 10:00:36 +0100

    ygf

--- pracownicy/funkcjeagregujace.php
+++ pracownicy/funkcjeagregujace.php
@@ -14,15 +14,15 @@
           <div class="tomek">
            <div class="navigator">
                  <nav>
-            <a class="nav1" href="index.php">Strona główna</a>
-            <a class="nav1" href="pracownicy.php">Pracownicy</a>
-            <a class="nav1" href="pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
-            <a class="nav1 active" href="funkcjeagregujace.php">Funkcje Agregujące</a>
+            <a class="nav1" href="../index.php">Strona główna</a>
+            <a class="nav1" href="/pracownicy/pracownicy.php">Pracownicy</a>
+            <a class="nav1" href="/pracownicy/pracownicyiorganizacja.php">Pracownicy i Organizacja</a>
+            <a class="nav1 active" href="/pracownicy/funkcjeagregujace.php">Funkcje Agregujące</a>
                     <div class="orga1">
-            <a class="nav3" href="funkcjeagregujace.php">Funkcje</a>
-            <a class="nav3" href="funkcjeagregujace.php">Group By</a>
-            <a class="nav3" href="funkcjeagregujace.php">Having By</a>
-            <a class="nav3" href="funkcjeagregujace.php">Data i Czas</a>
+            <a class="nav3" href="/pracownicy/funkcjeagregujace.php">Funkcje</a>
+            <a class="nav3" href="/pracownicy/funkcjeagregujace.php">Group By</a>
+            <a class="nav3" href="/pracownicy/funkcjeagregujace.php">Having By</a>
+            <a class="nav3" href="/pracownicy/funkcjeagregujace.php">Data i Czas</a>
                     </div>
                 </nav>
                 <div class="pracow">
