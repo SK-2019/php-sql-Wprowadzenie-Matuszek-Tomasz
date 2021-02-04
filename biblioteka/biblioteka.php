a07865896255f52d93de5eab1a20945ee74b3929
Bucky Roberts <matuszek.tomasz1@gmail.com>
2021-02-04 10:13:26 +0100

    vg

--- biblioteka/biblioteka.php
+++ biblioteka/biblioteka.php
@@ -15,3 +15,19 @@
            <div class="navigator">
                  <nav>
             <a class="nav1 active" href="index.php">Strona główna</a>
+            </div>
+            <h2 class="h2tm">Tomasz Matuszek</h2>
+              <div class="pracow">
+                  <?php
+              $conn= new mysqli("remotemysql.com","uAryroT03t","gOUYLSNMoE","uAryroT03t");
+            $result=$conn->query('SELECT * FROM biblAutor');
+        echo("<table style='margin-left: 5%;' border=2>");
+        echo("<th>ID</th>");
+        echo("<th>AUTOR</th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>" .$row["id"]. "</td><td>" .$row["autor"]. "</td>");
+                echo("</tr>");
+            }
+        echo("</table>");
+            ?>
