8a2be735c62927207afbafd1f697eba6014183e1
Bucky Roberts <matuszek.tomasz1@gmail.com>
2021-02-04 09:54:22 +0100

    fds

---
+++ biblioteka/biblioteka.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Matuszek</title>
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+        <header>
+                <div class="git">
+            <h3><a href="https://github.com/SK-2019/PHP-SQL-Wprowadzenie-Matuszek-Tomasz">GITHUB</h3>
+                </div>
+          <div class="tomek">
+           <div class="navigator">
+                 <nav>
+            <a class="nav1 active" href="index.php">Strona główna</a>
