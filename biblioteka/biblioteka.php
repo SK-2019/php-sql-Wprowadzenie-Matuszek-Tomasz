6805c33887f1fccd51ba80d9feba6549134b768d
Bucky Roberts <matuszek.tomasz1@gmail.com>
2021-02-04 11:02:25 +0100

    hgfj

--- biblioteka/biblioteka.php
+++ biblioteka/biblioteka.php
@@ -18,24 +18,58 @@
             </div>
             <h2 class="h2tm">Tomasz Matuszek</h2>
               <div class="pracow">
-        <?php
-        require_once("../assets/connect.php");
-  $sql = ('SELECT * FROM bibliotekaAT, bibliotekaAutor, bibliotekaTytul WHERE id_autor=bibliotekaAutor_ID AND id_tytuł=bibliotekaTytul_ID');
-  echo("<p>Wyświetlenie pełnej bazy danych w blibiotece</h1>");
+              <?php
+        
   
-  $result=$conn->query($sql);
-  echo("<table>");
-  echo("<th>id</th>");
-  echo("<th>Autor</th>");
-  echo("<th>Tytuł</th>");
-  while($row=$result->fetch_assoc())
-  {
-      echo("<tr>");
-      echo("<td>".$row["id"]."</td><td>".$row["Autor"]."</td><td>".$row["Tytuł"]);
-      echo("</tr>");
-  }
-  echo("</table>");
-            ?>
+        require_once('../assets/connect.php');
+       echo("<h1>Książki i ich autorzy:</h1>");
+       $sql = 'SELECT * FROM biblTytul';
+       echo("<h2>Tytuły:</h2>");
+       echo("<li>".$sql);
+       
+        $result = $conn->query($sql);
+             
+             echo("<select name='title' id='title'>");
+       while($row=$result->fetch_assoc()){ 
+       echo("<option value=".$row['id'].">".$row['tytul']."</option>");
+               }
+           echo("</select>");
+               
+               
+           
+           
+       $sql = 'SELECT * FROM biblAutor';
+       echo("<h2>Autorzy:</h2>");
+       echo("<li>".$sql);
+       
+        $result = $conn->query($sql);
+             echo("<select name='title' id='title'>");
+       while($row=$result->fetch_assoc()){
+       echo("<option value=".$row['id'].">".$row['autor']."</option>");
+               }
+           echo("</select>");
+           
+           $sql = 'SELECT * FROM biblAutor, biblTytul, biblAutor_biblTytul WHERE biblAutor_id=biblAutor.id and biblTytul_id=biblTytul.id';
+       echo("<h2>Łączna tabelka:</h2>");
+       echo("<li>".$sql);
+       
+        $result = $conn->query($sql);
+               echo("<table border=1>");
+               echo("<th>ID</th>");
+               echo("<th>Autor</th>");
+               echo("<th>Tytuł</th>");
+                   while($row=$result->fetch_assoc()){ 
+                       echo("<tr>");
+                           echo("<td>".$row["id"]."</td><td>".$row["autor"]."</td><td>".$row["tytul"]."</td>"); 
+                       echo("</tr>");
+                   }
+       
+               echo("</table>");
+               
+               
+               
+       
+       ?>
             </div>
     </header>
 </body>
