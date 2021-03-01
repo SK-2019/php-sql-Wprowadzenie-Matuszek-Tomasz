7caa7614c42faca93dacc310994df8b7d84b2843
Bucky Roberts <matuszek.tomasz1@gmail.com>
2021-03-01 11:50:17 +0100

    jk

--- assets/connect.php
+++ assets/connect.php
@@ -1,8 +1,12 @@
 <?php
-$servername = "remotemysql.com";
-$username = "uAryroT03t";
-$password = "gOUYLSNMoE";
-$dbname = "uAryroT03t";
+$hostname = $_SERVER['HTTP_HOST'];
+if ($hostname === '127.0.0.1'){
+    require_once("config.php");
+}
+$servername = $_SERVER['mysql'];
+$username = $_SERVER['nazwa'];
+$password = $_SERVER['haslo'];
+$dbname = $_SERVER['dbname'];
 
 $conn=new mysqli($servername, $username, $password, $dbname);
         if($conn->connect_error) {
